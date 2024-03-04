bb2a4175106af66e6779f7f445198881773322a3
devcom012 <162178870+devcom012@users.noreply.github.com>
2024-03-05 01:29:39 +0530

    Initial commit

---
+++ models/ecommerce/product.models.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose"
+import { Category } from "./category.models"
+
+const productSchema = new mongoose.Schema({
+  description:{
+    type:String,
+    required:true
+  },
+  name:{
+    type:String,
+    required:true
+  },
+  stock:{
+    type:Number,
+    default:0
+  },
+  price:{
+    type:Number,
+    default:0
+  },
+  category:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"Category",
+    required:true
+  },
+  owner:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"User"
+  }
+},{timestamps:true})
+
+export const Product= mongoose.model("Product",productSchema)
